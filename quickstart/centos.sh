7e7d66390e6af2f8d27e501cdcd80024f03a3c33
Eric Range <eric.range@enginsight.com>
2019-11-13 17:25:07 +0100

    Update

---
+++ quickstart/centos.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit 1
+fi
+
+if ! [ -x "$(which docker)" ]; then
+  sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+  sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+  sudo yum install -y docker-ce docker-ce-cli containerd.io
+  sudo systemctl enable docker
+  sudo systemctl start docker
+fi
+
+if ! [ -x "$(which -v docker-compose)" ]; then
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+fi
+
+if ! [ -x "$(which -v git)" ]; then
+  sudo yum install -y git
+fi
+
+git clone https://github.com/enginsight/enterprise
+cd enterprise
+
+#clear
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+
+#clear
+chmod +x ./setup.sh && (cd .; source ./setup.sh docker)
