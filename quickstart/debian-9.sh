a1f245386ec0e165aa87c9bca281a27750b8d37d
Eric Range <eric.range@enginsight.com>
2019-11-13 16:51:37 +0100

    Update

--- quickstart/debian-9.sh
+++ quickstart/debian-9.sh
@@ -6,29 +6,29 @@ if [ "$EUID" -ne 0 ]
 fi
 
 if ! [ -x "$(command -v docker)" ]; then
-  sudo apt update
-	sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
-	sudo apt update
-	sudo apt install -y docker-ce
+  sudo apt update > /dev/null
+	sudo apt install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common > /dev/null
+	curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - > /dev/null
+	sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" > /dev/null
+	sudo apt update > /dev/null
+	sudo apt install -y docker-ce > /dev/null
 fi
 
 if ! [ -x "$(command -v docker-compose)" ]; then
-  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-	sudo chmod +x /usr/local/bin/docker-compose
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose > /dev/null
+	sudo chmod +x /usr/local/bin/docker-compose > /dev/null
 fi
 
 if ! [ -x "$(command -v git)" ]; then
-  sudo apt update
-	sudo apt install -y git
+  sudo apt update > /dev/null
+	sudo apt install -y git > /dev/null
 fi
 
 git clone https://github.com/enginsight/enterprise
 cd enterprise
 
 #clear
-echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin > /dev/null
 
 #clear
 chmod +x ./setup.sh && (cd .; source ./setup.sh docker)
